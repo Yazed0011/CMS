a1f104e9d30a7068ba8eef3fd6702a26ea12b69f
Yazed0011 <yazedfouda@gmail.com>
2026-04-01 19:34:18 +0200

    Add \ modified

--- Model/Comments.php
+++ Model/Comments.php
@@ -37,5 +37,52 @@ class Comments{
                 error_log($e->getMessage());
                 throw new \Exception("ERROR SERVER", 500);
             }
-}
+
+        }
+        public function GetAllComment(){
+            try{
+                $stmt= $this->conn->prepare("SELECT content , created_at , user.name FROM comments 
+                INNER JOIN user ON comments.user_id = user.id");
+                $stmt->execute();
+                $comment=$stmt->fetchall();
+                if(count($comment) == 0){
+                    return json_encode([
+                        "success" => false,
+                        "message" => "Not Found"
+                    ]);
+                }
+                return json_encode([
+                    "success" =>true,
+                    "data" => $comment
+                ]);
+            }
+            catch(PDOException $e){
+                error_log($e->getMessage());
+                throw new \Exception("ERROR SERVER" , 500);
+            }
+        }
+
+        public function UpdateComment(array $data){
+            try{
+                $stmt= $this->conn->prepare("UPDATE comments SET content= :content , user_id= :user_id WHERE id = :id");
+                $stmt->bindParam(":content" , $data['content'] , PDO::PARAM_STR);
+                $stmt->bindParam(":user_id" , $data['user_id'] , PDO::PARAM_INT);
+                $stmt->bindParam(":id" , $data['id'] , PDO::PARAM_INT);
+                $stmt->execute();
+                if($stmt->rowCount() > 0){
+                    return json_encode([
+                        "success" => true,
+                        "message" => "Comment Update Successfully"
+                    ]);
+                }
+                    else {
+                        throw new \Exception("Failed to Update Comment", 400);
+                    }
+                
+            }
+            catch(PDOException $e){
+                error_log($e->getMessage());
+                throw new \Exception("ERROR SERVER" , 500);
+            }
+        }
 }
---
+++ valdition/CommentValidit.php
@@ -0,0 +1,6 @@
+<?php 
+namespace Validate\Comment;
+
+class ValidComment{
+    
+}
